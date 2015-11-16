8d27f3ec3dc812811411f489bc3dfa0ebdc98ca8
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-11-16 14:10:52 +0100

    Test with container infra

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,6 @@ git submodule update --init
 if [ -z $AUGEAS ]; then
   # Use latest version of lenses
   cd augeas && git pull origin master
-  PKG_VERSION=""
 else
   if [ -z $LENSES ]; then
     # Use matching version of lenses
@@ -15,21 +14,7 @@ else
   else
     cd augeas && git fetch && git checkout $LENSES
   fi
-
-  PKG_VERSION="=${AUGEAS}*"
-  # Add PPA
-  sudo add-apt-repository -y ppa:raphink/augeas-1.0.0
-  sudo add-apt-repository -y ppa:raphink/augeas-1.1.0
-  sudo add-apt-repository -y ppa:raphink/augeas-1.2.0
-  sudo add-apt-repository -y ppa:raphink/augeas-1.3.0
 fi
-sudo add-apt-repository -y ppa:raphink/augeas
-sudo apt-get update
-sudo apt-get install augeas-tools${PKG_VERSION} \
-                     augeas-lenses${PKG_VERSION} \
-                     libaugeas0${PKG_VERSION} \
-                     libaugeas-dev${PKG_VERSION} \
-                     libxml2-dev
 
 # Install gems
 gem install bundler
